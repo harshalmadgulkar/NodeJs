9c92fbbef6db07f0b3fde6a020d7d4807055b768
harshalmadgulkar <106461468+harshalmadgulkar@users.noreply.github.com>
2024-11-15 20:59:18 +0530

    chapter12 using winston logger

--- 12.Handling Errors/E-COM-API/src/middlewares/logger.middleware.js
+++ 12.Handling Errors/E-COM-API/src/middlewares/logger.middleware.js
@@ -1,20 +1,29 @@
-import fs from "fs";
-const fsPromise = fs.promises;
+// import fs from "fs";
+// const fsPromise = fs.promises;
+import winston from "winston";
 
-async function log(logData) {
-  try {
-    logData = `\n ${new Date().toString()} - ${logData}`;
-    await fsPromise.appendFile("log.txt", logData);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function log(logData) {
+//   try {
+//     logData = `\n ${new Date().toString()} - ${logData}`;
+//     await fsPromise.appendFile("log.txt", logData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "requet-logging" },
+  transports: [new winston.transports.File({ filename: "logs.txt" })],
+});
 
 const loggerMiddleware = async (req, res, next) => {
   // 1. log request body
   if (!req.url.includes("signin")) {
     const logData = `${req.url} - ${JSON.stringify(req.body)}`;
-    await log(logData);
+    // await log(logData);
+    logger.info(logData);
   }
   next();
 };
