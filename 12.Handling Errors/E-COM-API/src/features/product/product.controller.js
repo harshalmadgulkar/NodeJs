768af1f074e15557f865d62efb105558b97ed7c5
harshalmadgulkar <106461468+harshalmadgulkar@users.noreply.github.com>
2024-11-22 14:28:21 +0530

    chapter12 Error handling in Express

--- 12.Handling Errors/E-COM-API/src/features/product/product.controller.js
+++ 12.Handling Errors/E-COM-API/src/features/product/product.controller.js
@@ -1,4 +1,4 @@
-import ProductModel from './product.model.js';
+import ProductModel from "./product.model.js";
 
 export default class ProductController {
   getAllProducts(req, res) {
@@ -11,12 +11,10 @@ export default class ProductController {
     const newProduct = {
       name,
       price: parseFloat(price),
-      sizes: sizes.split(','),
+      sizes: sizes.split(","),
       imageUrl: req.file.filename,
     };
-    const createdRecord = ProductModel.add(
-      newProduct
-    );
+    const createdRecord = ProductModel.add(newProduct);
     res.status(201).send(createdRecord);
   }
 
@@ -24,26 +22,20 @@ export default class ProductController {
     const userID = req.query.userID;
     const productID = req.query.productID;
     const rating = req.query.rating;
-    const error = ProductModel.rateProduct(
-      userID,
-      productID, 
-      rating
-      );
-      console.log(error);
-      if(error) {
-        return res.status(400).send(error);
-      }else{
-        return res
-        .status(200)
-        .send('Rating has been added');
-      }
+    try {
+      ProductModel.rateProduct(userID, productID, rating);
+    } catch (err) {
+      // console.log(err);
+      return res.status(400).send(err.message);
+    }
+    return res.status(200).send("Rating has been added");
   }
 
   getOneProduct(req, res) {
     const id = req.params.id;
     const product = ProductModel.get(id);
     if (!product) {
-      res.status(404).send('Product not found');
+      res.status(404).send("Product not found");
     } else {
       return res.status(200).send(product);
     }
@@ -53,11 +45,7 @@ export default class ProductController {
     const minPrice = req.query.minPrice;
     const maxPrice = req.query.maxPrice;
     const category = req.query.category;
-    const result = ProductModel.filter(
-      minPrice,
-      maxPrice,
-      category
-    );
+    const result = ProductModel.filter(minPrice, maxPrice, category);
     res.status(200).send(result);
   }
 }
--- 12.Handling Errors/E-COM-API/src/features/product/product.model.js
+++ 12.Handling Errors/E-COM-API/src/features/product/product.model.js
@@ -1,15 +1,7 @@
-import UserModel from '../user/user.model.js';
+import UserModel from "../user/user.model.js";
 
 export default class ProductModel {
-  constructor(
-    id,
-    name,
-    desc,
-    price,
-    imageUrl,
-    category,
-    sizes
-  ) {
+  constructor(id, name, desc, price, imageUrl, category, sizes) {
     this.id = id;
     this.name = name;
     this.desc = desc;
@@ -26,9 +18,7 @@ export default class ProductModel {
   }
 
   static get(id) {
-    const product = products.find(
-      (i) => i.id == id
-    );
+    const product = products.find((i) => i.id == id);
     return product;
   }
 
@@ -39,88 +29,83 @@ export default class ProductModel {
   static filter(minPrice, maxPrice, category) {
     const result = products.filter((product) => {
       return (
-        (!minPrice ||
-          product.price >= minPrice) &&
-        (!maxPrice ||
-          product.price <= maxPrice) &&
-        (!category ||
-          product.category == category)
+        (!minPrice || product.price >= minPrice) &&
+        (!maxPrice || product.price <= maxPrice) &&
+        (!category || product.category == category)
       );
     });
     return result;
   }
 
-  static rateProduct(userID, productID, rating){
+  static rateProduct(userID, productID, rating) {
     // 1. Validate user and product
-    const user = UserModel.getAll().find(
-      (u) => u.id == userID
-    );
-    if(!user){
-      return "User not found";
+    const user = UserModel.getAll().find((u) => u.id == userID);
+    if (!user) {
+      // Error handling
+      throw new Error("User not found");
+      // return "User not found";
     }
 
     // Validate Product
-    const product = products.find(
-      (p) => p.id == productID);
-      if(!product){
-        return "Product not found";
-      }
+    const product = products.find((p) => p.id == productID);
+    if (!product) {
+      // console.log("product");
+      throw new Error("Product not found");
+    }
 
-      // 2. Check if there are any ratings and if not then add ratings array.
-      if(!product.ratings){
-        product.ratings = [];
+    // 2. Check if there are any ratings and if not then add ratings array.
+    if (!product.ratings) {
+      product.ratings = [];
+      product.ratings.push({
+        userID: userID,
+        rating: rating,
+      });
+    } else {
+      // 3. Check if user rating is already available.
+      const existingRatingIndex = product.rating.findIndex(
+        (r) => r.userID == userID
+      );
+      if (existingRatingIndex >= 0) {
+        product.ratings[existingRatingIndex] = {
+          userID: userID,
+          rating: rating,
+        };
+      } else {
+        // 4. if no exisitng rating, then add new rating.
         product.ratings.push({
-          userID:userID, 
-          rating: rating
+          userID: userID,
+          rating: rating,
         });
       }
-      else{
-        // 3. Check if user rating is already available.
-        const existingRatingIndex = product.rating.findIndex(
-          (r) => r.userID == userID
-        );
-        if(existingRatingIndex >= 0){
-          product.ratings[existingRatingIndex] = {
-            userID:userID, 
-            rating: rating,
-          };
-        }
-        else{
-          // 4. if no exisitng rating, then add new rating.
-          product.ratings.push({
-            userID:userID, 
-            rating: rating
-          });
-        }
-      }
+    }
   }
 }
 
 var products = [
   new ProductModel(
     1,
-    'Product 1',
-    'Description for Product 10',
+    "Product 1",
+    "Description for Product 10",
     19.99,
-    'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
-    'Category1'
+    "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    "Category1"
   ),
   new ProductModel(
     2,
-    'Product 2',
-    'Description for Product 2',
+    "Product 2",
+    "Description for Product 2",
     29.99,
-    'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg',
-    'Category2',
-    ['M', 'XL']
+    "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg",
+    "Category2",
+    ["M", "XL"]
   ),
   new ProductModel(
     3,
-    'Product 3',
-    'Description for Product 3',
+    "Product 3",
+    "Description for Product 3",
     39.99,
-    'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg',
-    'Category3',
-    ['M', 'XL', 'S']
+    "https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg",
+    "Category3",
+    ["M", "XL", "S"]
   ),
 ];
