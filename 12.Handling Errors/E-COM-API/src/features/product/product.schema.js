47894cf478969fbdb3e0718332118930c1959dda
Harshal Madgulkar <harshal.madgulkar725@gmail.com>
2024-12-20 19:09:54 +0530

    chapter17 Creating Schemas

---
+++ 12.Handling Errors/E-COM-API/src/features/product/product.schema.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const productSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  price: Number,
+  imageUrl: String,
+  category: String,
+  sizes: String,
+});
