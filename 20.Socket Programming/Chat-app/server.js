bf62e9ac996b2f2be34e6687c87f2854b0b370c8
Harshal <harshal.madgulkar725@gmail.com>
2025-02-10 16:00:02 +0530

    chapter20 Socket: Establishing Connection

---
+++ 20.Socket Programming/Chat-app/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+import http, { createServer } from "http";
+
+const app = express();
+
+// 1. Create Server using http
+const server = http.createServer(app);
+
+// 2. Create socket server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+// 3. Use socket events.
+io.on("connection", (socket) => {
+  console.log("Connection is established.");
+  socket.on("disconnect", () => {
+    console.log("Connection is disconnected.");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("App is listening on 3000");
+});
